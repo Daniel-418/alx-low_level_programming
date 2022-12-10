abc9b827da9d373e0516bc50712debd161541ec9
Daniel <komolafedanielolaoluwa@gmail.com>
2022-12-09 17:51:45 -0800

    9. Patience, persistence and perspiration make and unbeatable combination for success.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 999999; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
+
+		if (i == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
 
 	return (0);
