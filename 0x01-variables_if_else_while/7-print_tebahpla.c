799202c42159908901a6047857396a050fddbd17
Daniel <komolafedanielolaoluwa@gmail.com>
2022-12-09 17:03:35 -0800

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - Prints the alphabets in lowercase
+  *
+  * Return: 0 (Always success)
+  */
+int main(void)
+{
+	char alphabets[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+	'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+	'u', 'v', 'w', 'x', 'y', 'z'};
+	int a;
+
+	for (a = 25; a >= 0; a--)
+	{
+		putchar(alphabets[a]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
