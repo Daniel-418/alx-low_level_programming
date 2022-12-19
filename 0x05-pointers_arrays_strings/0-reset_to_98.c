6c2ae525cff716db8f9fb9848c047bf86c77a66e
Daniel <komolafedanielolaoluwa@gmail.com>
2022-12-19 15:42:42 -0800

    98 Battery st. fix-1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - Updates a pointer value
- * @*n: The pointer whose value is to be updated
+ * @n: The pointer whose value is to be updated
  *
  * Return: void (This function returns no value)
  */
