fc810115db566d11c7d9c1059c064d113bef2c32
Daniel <komolafedanielolaoluwa@gmail.com>
2022-12-19 15:40:14 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates a pointer value
+ * @*n: The pointer whose value is to be updated
+ *
+ * Return: void (This function returns no value)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
