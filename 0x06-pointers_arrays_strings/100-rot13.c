beba7136eb5998afbb580fd44c9306c0d3d3a580
Daniel <komolafedanielolaoluwa@gmail.com>
2022-12-22 19:16:11 -0800

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: The string to be encoded
+ *
+ * Return: The encoded string
+ */
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = (str[i] - 'a' + 13) % 26 + 'a';
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = (str[i] - 'A' + 13) % 26 + 'A';
+	}
+
+	return (str);
+}
