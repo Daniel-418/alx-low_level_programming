d98d73b5b7a825f35c209ec36d4bdc9b9def59ef
Daniel <komolafedanielolaoluwa@gmail.com>
2023-01-05 09:43:05 -0800

    4. Once and idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculates the value of x raised to the power of y
+ * @x: the base of the calculation
+ * @y: the exponent of the calculation
+ *
+ * Return: the result of the exponential calculation
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
+	else if (y < 0)
+		return 1 / _pow_recursion(x, -y);
+	else
+		return (x * _pow_recursion(x, (y - 1)));
+}
