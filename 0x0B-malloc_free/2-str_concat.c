fc1935e5349693866741638372c651340dd0778d
Daniel <komolafedanielolaoluwa@gmail.com>
2023-01-09 05:25:48 -0800

    2. He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+int strlength(char *s);
+
+/**
+ * str_concat - Function that concatenates two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: A pointer to a newly allocated space in memory which contains
+ * the contents of s1 followed by the contents of s2.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *newstring;
+	int i, j, s1length, s2length;
+
+	if (s1 == 0)
+		*s1 = '\0';
+	if (s2 == 0)
+		*s2 = '\0';
+
+	s1length = strlength(s1);
+	s2length = strlength(s2);
+
+	newstring = malloc((sizeof(char) * (s1length + s2length)) + 2);
+
+	if (newstring == 0)
+		return (0);
+
+	for (i = 0; i < s1length; i++)
+		newstring[i] = s1[i];
+
+	for (j = 0; j < s2length; j++)
+		newstring[j + i] = s2[j];
+
+	return (newstring);
+}
+
+/**
+ * strlength - finds the length of a string
+ * @s: the string which length is to be found
+ *
+ * Return: the length of the string
+ */
+int strlength(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
