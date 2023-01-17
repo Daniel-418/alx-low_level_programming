f2d5492d80946e6f8a0d411e2dfc625ffda00d76
Daniel <komolafedanielolaoluwa@gmail.com>
2023-01-16 16:14:07 -0800

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef GUARD
+#define GUARD
+/**
+ * struct dog - contains all the dog variables
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
