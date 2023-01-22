e3f9631e97bafb9b367eecebc66c10462ed1ce01
Daniel <komolafedanielolaoluwa@gmail.com>
2023-01-22 10:51:51 -0800

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include "function_pointers.h"
+
+/**
+ * get_op_func - gets the operator function
+ * @s: operation to perform
+ *
+ * Return: pointer to the required function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{0, 0}
+	};
+	int i;
+
+	i = 0;
+	while(ops[i].op)
+	{
+		if (*s == *(ops[i].op))
+			return (ops[i].f);
+
+		i++;
+	}
+
+	return (0);
+}
