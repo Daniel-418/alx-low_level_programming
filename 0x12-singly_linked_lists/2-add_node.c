477d00c4595f612f331ed1f8b567f6f980967488
Daniel <komolafedanielolaoluwa@gmail.com>
2023-02-02 14:36:57 -0800

    2. Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "lists.h"
+#include "string.h"
+
+unsigned int _strlen(char *str);
+
+/**
+ * add_node - adds a node to the beginning of a list
+ * @head: The list head
+ * @str: The data to be added
+ *
+ * Return: pointer to the beginning of the list if sucessful, 0 if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == 0)
+		return (0);
+
+	new->str = strdup(str);
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+
+	if (new->str == 0 || new->len == 0 || new->next == 0)
+		return (0);
+
+	return (new);
+}
+
+/**
+ * _strlen - calculates the length of a string
+ * @str: the string to be calculated
+ *
+ * Return: the length of the string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int count;
+
+	for (count = 0; str[count] != '\0'; count++)
+		str[count] = str[count];
+
+	return (count);
+}
