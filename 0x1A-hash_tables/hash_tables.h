67488f8f5f9d3cac068fc1c5a002972a033d5161
Daniel <komolafedanielolaoluwa@gmail.com>
2023-04-07 13:51:46 -0700

    finished task __0.>>>ht = {}

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -5,7 +5,7 @@
 
 /**
  * struct hash_node_s - Node of a hash table
- * 
+ *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
@@ -16,7 +16,7 @@ typedef struct hash_node_s
 	char *key;
 	char *value;
 	struct hash_node_s *next;
-}hash_node_t;
+} hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -31,7 +31,7 @@ typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
-}hash_table_t;
+} hash_table_t;
 
 /**Prototype of functions **/
 hash_table_t *hash_table_create(unsigned long int size);
