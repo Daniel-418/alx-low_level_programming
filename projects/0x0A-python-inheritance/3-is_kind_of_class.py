165fe8418b9aa2f76351cb951af63199d32507c7
Daniel <komolafedanielolaoluwa@gmail.com>
2023-04-30 12:36:16 -0700

    removed the project not yet available to me for the new cohort

---
+++ projects/0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""is kind of class"""
+
+def is_kind_of_class(obj, a_class):
+    """Checks if an object is an instance or inherits from a class"""
+
+    return isinstance(obj, a_class)
---
+++ projects/0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""base geometry class is defined here"""
+
+class BaseGeometry:
+    """An empty class"""
+
+    def area(self):
+        """A method that has not been implemented"""
+        raise Exception("area() is not implemented")
