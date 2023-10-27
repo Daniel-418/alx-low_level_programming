a252885b7da2dd225f4e230b061312f227dd46d5
Daniel <komolafedanielolaoluwa@gmail.com>
2023-10-26 19:23:14 -0700

    Completed the first task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,15 +4,16 @@
  * binary_to_uint - Converts a binary number to an integer
  * @b: The binary number to be converted
  *
- * Return: the converted number number or 0 if 
- * 			b is null, if there is a char in b that is not 0
- * 			or 1
+ * Return: the converted number number or 0 if
+ * b is null, if there is a char in b that is not 0
+ * or 1
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int decimalNumber;
 	int stringLength;
 	int i;
+
 	if (b == NULL)
 		return (0);
 
@@ -25,7 +26,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] - '0' == 0)
 			decimalNumber <<= 1;
-		else if(b[i] - '0' == 1)
+		else if (b[i] - '0' == 1)
 			decimalNumber = (decimalNumber << 1) | 1;
 		else
 			return (0);
@@ -33,5 +34,5 @@ unsigned int binary_to_uint(const char *b)
 		stringLength--;
 	}
 
-	return(decimalNumber);
+	return (decimalNumber);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,2 +1,9 @@
+#ifndef GUARD_H
+#define GUARD_H
+
 #include <unistd.h>
+
 int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif
