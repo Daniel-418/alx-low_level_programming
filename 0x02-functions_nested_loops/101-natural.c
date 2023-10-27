6db8f272a4ba68fe152a79e02061e7ac9c07ab20
Daniel <komolafedanielolaoluwa@gmail.com>
2023-10-26 22:53:03 -0700

    did the final task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints out all the sums of 3 and 5
+ * @void: This function takes no arguments
+ *
+ * @Return: 0 if successful, 1 if fail
+ */
+int main(void)
+{
+	int i;
+	int total;
+
+	total = 0;
+	for (i = 1; i < 1024; ++i)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			total += i;
+	}
+
+	printf("%d\n", total);
+	return (0);
+}
